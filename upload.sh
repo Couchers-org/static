9d28ac308c08aa744a12103d0960791657710ee4
Aapeli <git@aapelivuorinen.com>
2025-05-25 11:41:10 -0400

    Add mansalva

--- upload.sh
+++ upload.sh
@@ -5,4 +5,4 @@ aws s3 sync ./maps s3://cdn.couchers.org/maps --delete
 aws s3 cp index.html s3://cdn.couchers.org
 aws s3 cp error.html s3://cdn.couchers.org
 
-aws s3 sync fonts s3://cdn.couchers.org/fonts
+aws s3 sync ./fonts s3://cdn.couchers.org/fonts
