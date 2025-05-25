3fe76dbb1937d5674b6ab6c72daf52f0889a4a02
Aapeli <git@aapelivuorinen.com>
2025-05-25 11:17:24 -0400

    Add fonts

---
+++ fonts/ubuntu/readme.md
@@ -0,0 +1,1 @@
+Note that these files are used on the platform and in emails sent via the backend.
--- upload.sh
+++ upload.sh
@@ -1,3 +1,8 @@
-export $(xargs <.env)
+export AWS_PROFILE=couchers
 
-aws s3 sync ./static s3://cdn.couchers.org/maps --delete
+aws s3 sync ./maps s3://cdn.couchers.org/maps --delete
+
+aws s3 cp index.html s3://cdn.couchers.org
+aws s3 cp error.html s3://cdn.couchers.org
+
+aws s3 sync fonts s3://cdn.couchers.org/fonts
